43b38b84c989824b93c0226caa5c6811479347fc
Sajid Ali <thesajidali1997@gmail.com>
2024-08-13 23:21:11 +0500

    second commit

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Button, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import BallotPaperScreen from "./screens/BallotPaperScreen";
@@ -11,18 +11,24 @@ const Stack = createStackNavigator();
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Button
-        title="Ballot Paper"
+      <TouchableOpacity
+        style={styles.button}
         onPress={() => navigation.navigate("BallotPaper")}
-      />
-      <Button
-        title="Polling Station"
+      >
+        <Text style={styles.buttonText}>Ballot Paper</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
         onPress={() => navigation.navigate("PollingStation")}
-      />
-      <Button
-        title="For Assistance"
+      >
+        <Text style={styles.buttonText}>Polling Station</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
         onPress={() => navigation.navigate("Assistance")}
-      />
+      >
+        <Text style={styles.buttonText}>For Assistance</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -31,7 +37,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Vote For Change, Vote For Waseem Nazir"
+          component={HomeScreen}
+        />
         <Stack.Screen name="BallotPaper" component={BallotPaperScreen} />
         <Stack.Screen name="PollingStation" component={PollingStationScreen} />
         <Stack.Screen name="Assistance" component={AssistanceScreen} />
@@ -45,5 +54,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#f8f8f8",
+    padding: 20,
+  },
+  button: {
+    backgroundColor: "#1f8c07",
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 8,
+    marginVertical: 10,
+    width: "80%",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "600",
+    textAlign: "center",
   },
 });
--- screens/BallotPaperScreen.js
+++ screens/BallotPaperScreen.js
@@ -1,39 +1,121 @@
 import React, { useState } from "react";
-import { View, Picker, Button, Image, StyleSheet } from "react-native";
+import { View, Button, StyleSheet, Image, Dimensions } from "react-native";
+import { Picker } from "@react-native-picker/picker";
 
-const BallotPaperScreen = () => {
+const PollingStationScreen = () => {
   const [province, setProvince] = useState("");
   const [discipline, setDiscipline] = useState("");
+
+  const disciplinesArr = {
+    punjab: [
+      "Civil/Architectural/Ecological",
+      "Transport/Environmental/Urban/Ocean",
+      "Mechanical",
+      "Mecantro/Indus/Nuclear/Textile/Automotive",
+      "Metal/Agricultural/Mining/Pet-gas",
+      "Chemical/Polymer/Food",
+      "Computer",
+      "Telecom/Aero/Avionics",
+      "Electronics/Engg Science",
+      "Electrical/Bio-Medical/Energy",
+    ],
+    sindh: [
+      "Civil/Architectural/Ecological",
+      "Transport/Environmental/Urban/Ocean",
+      "Mechanical",
+      "Mecantro/Indus/Nuclear/Textile/Automotive",
+      "Metal/Agricultural/Mining/Pet-gas",
+      "Chemical/Polymer/Food",
+      "Computer",
+      "Telecom/Aero/Avionics",
+      "Electronics/Engg Science",
+      "Electrical/Bio-Medical/Energy",
+    ],
+    kpk: [
+      "Civil/Architectural/Ecological",
+      "Transport/Environmental/Urban/Ocean",
+      "Mechanical",
+      "Mecantro/Indus/Nuclear/Textile/Automotive",
+      "Metal/Agricultural/Mining/Pet-gas",
+      "Chemical/Polymer/Food",
+      "Computer",
+      "Telecom/Aero/Avionics",
+      "Electronics/Engg Science",
+      "Electrical/Bio-Medical/Energy",
+    ],
+    baloch: [
+      "Civil/Architectural/Ecological",
+      "Transport/Environmental/Urban/Ocean",
+      "Mechanical",
+      "Mecantro/Indus/Nuclear/Textile/Automotive",
+      "Metal/Agricultural/Mining/Pet-gas",
+      "Chemical/Polymer/Food",
+      "Computer",
+      "Telecom/Aero/Avionics",
+      "Electronics/Engg Science",
+      "Electrical/Bio-Medical/Energy",
+    ],
+    gb_azk: [
+      "Civil/Architectural/Ecological",
+      "Transport/Environmental/Urban/Ocean",
+      "Mechanical",
+      "Mecantro/Indus/Nuclear/Textile/Automotive",
+      "Metal/Agricultural/Mining/Pet-gas",
+      "Chemical/Polymer/Food",
+      "Computer",
+      "Telecom/Aero/Avionics",
+      "Electronics/Engg Science",
+      "Electrical/Bio-Medical/Energy",
+    ],
+  };
+
+  const disciplines = province ? disciplinesArr[province] : [];
+  const { width, height } = Dimensions.get("window");
+
+  const getImageSource = () => {
+    if (discipline && province) {
+      const imageName = `${discipline.toLowerCase()}_${province.toLowerCase()}.jpeg`;
+      return require(`../assets/ballot-paper-images/${imageName}`);
+    }
+    return null;
+  };
 
   return (
     <View style={styles.container}>
       <Picker
         selectedValue={province}
-        onValueChange={(itemValue) => setProvince(itemValue)}
+        onValueChange={(itemValue) => {
+          setProvince(itemValue);
+          setDiscipline("");
+        }}
+        style={styles.picker}
       >
         <Picker.Item label="Select Province" value="" />
         <Picker.Item label="Punjab" value="punjab" />
         <Picker.Item label="Sindh" value="sindh" />
         <Picker.Item label="KPK" value="kpk" />
         <Picker.Item label="Balochistan" value="balochistan" />
-        <Picker.Item label="GB/AZK" value="gb_azk" />
+        <Picker.Item label="Gilgal B./Azad Kash." value="gb_azk" />
       </Picker>
 
       <Picker
         selectedValue={discipline}
         onValueChange={(itemValue) => setDiscipline(itemValue)}
+        style={styles.picker}
+        enabled={province !== ""}
       >
         <Picker.Item label="Select Discipline" value="" />
-        <Picker.Item label="Mechanical" value="mechanical" />
-        <Picker.Item label="Electrical" value="electrical" />
-        <Picker.Item label="Civil" value="civil" />
+        {disciplines.map((cityName, index) => (
+          <Picker.Item key={index} label={cityName} value={cityName} />
+        ))}
       </Picker>
 
-      {province && discipline ? (
+      {discipline ? (
         <View>
           <Image
-            source={{ uri: "https://example.com/ballot.jpg" }} // Replace with your image URL
-            style={{ width: 300, height: 400 }}
+            source={getImageSource()}
+            style={{ width: width * 1, height: height * 0.6 }}
+            resizeMode="cover"
           />
           <Button
             title="Download"
@@ -51,6 +133,11 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  picker: {
+    height: 50,
+    width: 300,
+    marginVertical: 10,
+  },
 });
 
-export default BallotPaperScreen;
+export default PollingStationScreen;
